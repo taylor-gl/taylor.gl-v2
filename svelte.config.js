22145573a1d2f59c07a400cbd80fc85d59a14b4a
Taylor G. Lunt <taylor@taylor.gl>
2025-06-24 18:32:39 -0400

    Remove fallback URL and ensure full static-site generation
    This fixes a routing error when deployed to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: 'app.html',
-    }),
+    adapter: adapter(),
     prerender: {
       handleHttpError: ({ path, referrer, message }) => {
         // Ignore 404s for external links during prerendering
